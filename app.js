f693f8eb22fa04c38ce36f0c7a4257ddd9a6372a
Bora Eryilmaz <bora.eryilmaz@gmail.com>
2017-01-05 01:12:42 -0500

    requesting all_messages

--- app.js
+++ app.js
@@ -15,11 +15,12 @@
     console.log('Connection to LMU service disconnected');
   });
 
+  const IMEI = ['358683062687414', '358683062267480', '270113183009699599'];
   // Action Code (1:reboot, 9:ID, 10:locate, 15:state)
-  var IMEI = ['358683062687414', '358683062267480', '270113183009699599'];
-  const req =  {IMEI:IMEI[1], type:"report_request", code: 10};
-  // const req = {IMEI:"358683062267480", type:"last_location_message"};
+  // const req = {IMEI:IMEI[1], type:"report_request", code: 1};
+  // const req = {IMEI:IMEI[1], type:"last_location_message"};
+  const req = {IMEI:IMEI[1], type:"all_messages"};
   socket.emit('request', req, (data) => {
-    console.log(data);
+    console.log(require('util').inspect(data, false, null))
   });
 }());
