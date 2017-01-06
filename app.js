f2baaf44d358c6fece1ecb5bc113788dd8b3baa2
Bora Eryilmaz <bora.eryilmaz@gmail.com>
2017-01-06 02:17:57 -0500

    testing all-messsages-after request

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 "use strict";
 (function() {
   const url = 'http://ec2-107-22-152-194.compute-1.amazonaws.com:8000';
+  // const url = 'http://localhost:8000';
   const socket = require('socket.io-client')(url);
 
   socket.on('connect', () => {
@@ -17,9 +18,10 @@
 
   const IMEI = ['358683062687414', '358683062267480', '270113183009699599'];
   // Action Code (1:reboot, 9:ID, 10:locate, 15:state)
-  // const req = {IMEI:IMEI[1], type:"report_request", code: 1};
-  // const req = {IMEI:IMEI[1], type:"last_location_message"};
-  const req = {IMEI:IMEI[1], type:"all_messages"};
+  // const req = {IMEI:IMEI[2], type:'report_request', code: 10};
+  // const req = {IMEI:IMEI[1], type:'last_location_message'};
+  const req = {ID:'586ecf6e6234d91201c9b33b', IMEI:IMEI[1], type:'all_messages_after'};
+
   socket.emit('request', req, (data) => {
     console.log(require('util').inspect(data, false, null))
   });
