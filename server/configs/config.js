ef5ce55b584cdd3a56bd1256b463c41711daf688
Bora Eryilmaz <bora.eryilmaz@gmail.com>
2017-02-28 23:40:26 -0500

    env for production and stage servers

---
+++ server/configs/config.js
@@ -0,0 +1,13 @@
+// Copyright 2017 Bora Eryilmaz
+'use strict';
+
+let config = {};
+
+if (process.env.NODE_ENV &&
+    process.env.NODE_ENV === "STAGE") {
+  config = require('./server_config.json').stage;
+} else {
+  config = require('./server_config.json').production;
+}
+
+module.exports = config;
--- server/server.js
+++ server/server.js
@@ -2,39 +2,27 @@
 'use strict';
 
 const express = require('express');
+const config = require('./configs/config');
 
+/**
+ * Wires together server modules to build a demo application.
+ */
 class Server {
+  /**
+   * Starts a web application server.
+   */
   constructor() {
     this.startSocket();
     this.startServer();
   }
 
-  startSocket() {
-    const url = 'http://ec2-107-22-152-194.compute-1.amazonaws.com:8000';
-    // const url = 'http://localhost:8000';
-    this.device_cloud = require('socket.io-client')(url, {query: {'api_key': 'AS8GE5342G4GFD74'}});
-
-    this.device_cloud.on('connect', () => {
-      console.log('Connected to LMU service at', url);
-    });
-
-    this.device_cloud.on('message', (data) => {
-      console.log(`Message (Type: ${data.Message.Message_Header.Message_Type}, Event Code: ${data.Message.Message_Contents.Event_Code}) received from ESN:${data.ESN} (VIN:${data.VIN})`);
-      this.handle_LMU_message(data);
-    });
-
-    this.device_cloud.on('disconnect', () => {
-      console.log('Connection to LMU service disconnected');
-    });
-
-    this.device_cloud.on('error', (err) => {
-      console.log('Error:', err);
-    });
-  }
-
+  /**
+   * Starts the HTTP server and the web socket to handle communication
+   * with client web applications.
+   */
   startServer() {
     const app = express();
-    app.set('port', (process.env.PORT || 8001));
+    app.set('port', config.server.port);
     app.use(express.static(__dirname + '/../public'));
 
     // Start the HTTP server
@@ -57,7 +45,33 @@ class Server {
       socket.on('disconnect', () => {
         console.log(`Socket from ${address} disconnected.`);
       });
+
+      socket.on('error', (err) => {
+        console.log('Socket error:', err);
       });
+    });
+  }
+
+  startSocket() {
+    const url = `${config.device_cloud.url}:${config.device_cloud.port}`;
+    this.device_cloud = require('socket.io-client')(url, {query: {'api_key': config.device_cloud.api_key}});
+
+    this.device_cloud.on('connect', () => {
+      console.log('Connected to LMU service at', url);
+    });
+
+    this.device_cloud.on('message', (data) => {
+      console.log(`Message (Type: ${data.Message.Message_Header.Message_Type}, Event Code: ${data.Message.Message_Contents.Event_Code}) received from ESN:${data.ESN} (VIN:${data.VIN})`);
+      this.handle_LMU_message(data);
+    });
+
+    this.device_cloud.on('disconnect', () => {
+      console.log('Connection to LMU service disconnected');
+    });
+
+    this.device_cloud.on('error', (err) => {
+      console.log('Error:', err);
+    });
   }
 
   handle_client_request(req) {
