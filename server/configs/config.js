c6e666e3bbb3b90ddc92b3c2f55b4d5dc0d4be7d
Bora Eryilmaz <bora.eryilmaz@gmail.com>
2017-03-28 22:15:13 -0400

    default is production config

--- server/configs/config.js
+++ server/configs/config.js
@@ -4,15 +4,15 @@
 let config = {};
 
 switch (process.env.NODE_ENV) {
+default:
+case "production":
+  config = require('./server_config.json').production;
+  break;
 case "local":
   config = require('./server_config.json').local;
   break;
 case "stage":
   config = require('./server_config.json').stage;
-  break;
-case "production":
-default:
-  config = require('./server_config.json').production;
   break;
 }
 
