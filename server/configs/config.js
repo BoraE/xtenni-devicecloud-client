657b067ff2ff618f9166aa2be054fa64b2794093
Bora Eryilmaz <bora.eryilmaz@gmail.com>
2017-12-11 22:16:28 -0500

    config cleanup

--- server/configs/config.js
+++ server/configs/config.js
@@ -1,19 +1,15 @@
 // Copyright 2017 Bora Eryilmaz
 'use strict';
 
-let config = {};
+const sc = require('./server_config.json');
 
-switch (process.env.NODE_ENV) {
-default:
-case "production":
-  config = require('./server_config.json').production;
-  break;
-case "local":
-  config = require('./server_config.json').local;
-  break;
-case "stage":
-  config = require('./server_config.json').stage;
-  break;
-}
+let env = process.env.NODE_ENV || 'development';
+let common = sc.common;
+let config = sc[env];
+
+// Merge
+Object.assign(common.device_cloud, config.device_cloud);
+Object.assign(config, common);
+console.log(config);
 
 module.exports = config;
