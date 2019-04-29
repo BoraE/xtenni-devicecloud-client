d1f92271bfd392d50eb9d70d769705ffe1224b11
Bora Eryilmaz <bora.eryilmaz@gmail.com>
2019-04-28 22:22:50 -0400

    new deployment locations

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,15 +3,37 @@
 
 module.exports = {
   apps : [{
-    name: 'app_client',
+    name: 'xtenni-devicecloud-client',
     script: './app.js',
     autorestart: true,
-    watch: true,
+    watch: false,
     env: {
       NODE_ENV: 'development'
+    },
+    env_staging: {
+      NODE_ENV: 'staging'
     },
     env_production: {
       NODE_ENV: 'production'
     }
-  }]
+  }],
+
+  deploy: {
+    staging: {
+      user: "beryilma",
+      host: "boron",
+      repo: "git@github.com:BoraE/xtenni-devicecloud-client.git",
+      ref: "origin/master",
+      path: "/home/beryilma/Sandbox/Picotek/xtenni-devicecloud-client",
+      "post-deploy" : "npm set audit false && npm install && pm2 startOrRestart ecosystem.config.js --env staging"
+    },
+    production: {
+      user: "ubuntu",
+      host: "xtenni",
+      repo: "git@github.com:BoraE/xtenni-devicecloud-client.git",
+      ref: "origin/master",
+      path: "/home/ubuntu/Sandbox/Picotek/xtenni-devicecloud-client",
+      "post-deploy" : "npm set audit false && npm install && pm2 startOrRestart ecosystem.config.js --env production"
+    }
+  }
 };
